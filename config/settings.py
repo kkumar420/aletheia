61da5636356c835ded971b61c3f89441310d7889
kkumar420 <albusseverus31780@gmail.com>
2026-06-07 14:17:24 +0530

    Disabled WhiteNoise strict manifest to fix error

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,8 @@ STORAGES = {
 # The legacy variable left here explicitly to prevent the Cloudinary package from crashing
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ### ------------------------------------------ ###
+# Tell WhiteNoise not to crash if it can't find a file referenced in third-party CSS
+WHITENOISE_MANIFEST_STRICT = False
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
